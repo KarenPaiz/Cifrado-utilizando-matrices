528ebbce9d6441b995e39d705e893bdf7c6e5d7e
Karen Izabel <karenizabel1999@gmail.com>
2020-11-17 09:45:55 -0600

    Update Proyecto.asm
    EJERCICIOS 3 Y 4 (descifrado)

--- Proyecto.asm
+++ Proyecto.asm
@@ -32,6 +32,28 @@ Mapeo MACRO X, Y, Rows, Columns, Size
 	ADD AL, CL				; Resultado en AL
 ENDM
 
+
+Busca_posicion MACRO unaPosicion
+	LEA ESI, matriz
+	XOR ECX, ECX
+	MOV CL, unaPosicion
+	ADD ESI, ECX
+	MOV CL, [ESI]
+	MOV texto, CL
+	
+	LEA ESI, aux
+	XOR ECX, ECX
+	MOV CL, contador
+	ADD ESI, ECX
+	MOV CL, texto
+	MOV [ESI], cl
+
+
+	inc contador
+
+	INVOKE StdOut, ADDR texto
+ENDM
+
 .386
 .model flat, stdcall
 Option casemap: none
@@ -45,6 +67,10 @@ matriz     DB 690 DUP(0), 0
 Tab        DB  0Ah, 0
 value      DB 100 DUP(0), 0
 key        DB 100 DUP(0),0
+<<<<<<< Updated upstream
+=======
+cripto     DB 100 DUP(0),0
+>>>>>>> Stashed changes
 Count      DB 0,0
 count_row   DB 0,0
 Count_impri DW 0,0
@@ -53,6 +79,16 @@ Y          DB 0,0
 Y_Aux      DB 0,0
 Position   DB 0,0 
 num_recorrido DW 0,0
+<<<<<<< Updated upstream
+=======
+numOP2 DB 0,0
+numOP22 DB 0,0
+texto DB 0,0
+S DB 0,0
+ posicion	DB 0,0
+ contador DB 0,0
+ aux	 DB 100 DUP(0),0
+>>>>>>> Stashed changes
 .DATA?
  Option_     DB ?
  Ej_imprimir DB ? 
@@ -62,6 +98,10 @@ num_recorrido DW 0,0
  num_V       DB ?
  num         DW ?
  letra_cifrada DB ?
+<<<<<<< Updated upstream
+=======
+ 
+>>>>>>> Stashed changes
 .CONST ; Constantes
 ;CONTADORES
 Rows    DB 1Ah,0
@@ -72,19 +112,30 @@ main:
    CALL Generate_Matrix 
    ;CALL IMPRIMIR_MATRIZ                      ;solo para validar las que lo guarda bien
 
+<<<<<<< Updated upstream
 INVOKE StdOut, ADDR Tab
 print chr$("  Cifrado utilizando matrices  ")
+=======
+>>>>>>> Stashed changes
 INVOKE StdOut, ADDR Tab
-print chr$(" Ingresar Opcion ")
+print chr$("  ---Cifrado utilizando matrices---  ")
 INVOKE StdOut, ADDR Tab
-print chr$(" Opcion 1 ")
+print chr$("  ---------------------------------  ")
 INVOKE StdOut, ADDR Tab
-print chr$(" Opcion 2 ")
 INVOKE StdOut, ADDR Tab
-print chr$(" Opcion 3 ")
+print chr$("                --Menu-- ")
 INVOKE StdOut, ADDR Tab
-print chr$(" Opcion 4 ")
+print chr$(" Opcion 1: Generar criptograma con metodo 1 ")
 INVOKE StdOut, ADDR Tab
+print chr$(" Opcion 2: Generar criptograma con metodo 2 ")
+INVOKE StdOut, ADDR Tab
+print chr$(" Opcion 3: Descifrar criptograma con metodo 1 ")
+INVOKE StdOut, ADDR Tab
+print chr$(" Opcion 4: Descifrar criptograma con metodo 2 ")
+INVOKE StdOut, ADDR Tab
+print chr$(" Opcion 5: ? ")
+INVOKE StdOut, ADDR Tab
+print chr$(" Ingrese la opcion que desea ")
 INVOKE StdIn, ADDR Option_, 10
 ; convertir numero real
 SUB Option_, 30h 
@@ -113,11 +164,207 @@ Option_1:
  JMP EXIT_Main
 
 Option_2:
-
 Option_3:
+	print chr$(" Ingresar un criptograma (en MAYUSCULAS) ")
+	INVOKE StdOut, ADDR Tab
+	INVOKE StdIn, ADDR cripto, 100
+	print chr$(" Ingresar un clave (en MAYUSCULAS) ")
+	INVOKE StdOut, ADDR Tab
+	INVOKE StdIn, ADDR key, 100
 
+	LEA ESI,cripto
+	LEA EDI, key
+	MOV S, 0h
+OP2C1:
+	XOR BX, BX
+	MOV BL, [EDI]
+	CMP BL, 0
+	JNE SIO1
+	XOR EDI, EDI
+	LEA EDI, key
+	
+SIO1:	
+	XOR AX, AX
+	XOR BX, BX
+	MOV AL, [ESI]
+	CMP AL, 0
+	JE SALEOP3
+
+	MOV AL, [ESI]
+	MOV BL, [EDI]
+	CMP AL, 65
+	JAE SI1
+	ADD ESI, 1H
+	INC S
+	JMP OP2C1
+SI1:
+	CMP AL, 90
+	JBE SI2
+	ADD ESI, 1
+	INC S
+	JMP OP2C1
+SI2:
+	CMP BL, 65
+	JAE SI3
+	ADD EDI, 1H
+	JMP OP2C1
+SI3:
+	CMP  BL, 90
+	JBE OP2
+	ADD EDI, 1H
+	JMP OP2C1
+
+OP2:
+	XOR AX, AX
+	XOR BX, BX
+	XOR CX, CX
+	XOR DX, DX
+	MOV AL, [EDI]
+	MOV numOP2, AL
+	SUB numOP2, 65
+	MOV BL, [ESI]
+	SUB BL, 65
+	MOV numOP22, BL
+	MOV posicion, 0h
+	CMP BL, numOP2
+	JE OP2C2
+	CMP BL, numOP2
+	JA OP2C3
+	CMP BL, numOP2
+	JB OP2C4
+OP2C2: ;IGUALES
+	MOV AL, numOP2
+	MOV posicion, AL
+	JMP OP2C5
+OP2C3: ;MAYOR
+	MOV AL, numOP22
+	SUB AL, numOP2
+	MOV posicion, AL
+	JMP OP2C5
+OP2C4: ;Menor
+	MOV AL, 26
+	SUB AL, numOP2
+	ADD AL, numOP22
+	MOV posicion, AL
+OP2C5:
+
+
+	Busca_posicion posicion
+	XOR EAX, EAX
+	LEA ESI, cripto
+	MOV AL, S
+	ADD ESI, EAX
+
+	ADD EDI, 1
+	ADD ESI, 1
+	INC S
+
+	MOV AL, [ESI]
+	CMP AL, 0
+	JNE OP2C1
+SALEOP3:		
+JMP EXIT_Main
 Option_4:
 
+print chr$(" Ingresar un criptograma (en MAYUSCULAS) ")
+	INVOKE StdOut, ADDR Tab
+	INVOKE StdIn, ADDR cripto, 100
+	print chr$(" Ingresar un clave (en MAYUSCULAS) ")
+	INVOKE StdOut, ADDR Tab
+	INVOKE StdIn, ADDR key, 100
+
+	LEA ESI,cripto
+	LEA EDI, key
+	MOV S, 0h
+OP4C1:
+	XOR BX, BX
+	MOV BL, [EDI]
+	CMP BL, 0
+	JNE SIP1
+	XOR EDI, EDI
+	LEA EDI, aux
+	
+SIP1:	
+	XOR AX, AX
+	XOR BX, BX
+	MOV AL, [ESI]
+	CMP AL, 0
+	JE SALEOP4
+
+	MOV AL, [ESI]
+	MOV BL, [EDI]
+	CMP AL, 65
+	JAE SO1
+	ADD ESI, 1H
+	INC S
+	JMP OP4C1
+SO1:
+	CMP AL, 90
+	JBE SO2
+	ADD ESI, 1
+	INC S
+	JMP OP4C1
+SO2:
+	CMP BL, 65
+	JAE SO3
+	ADD EDI, 1H
+	JMP OP4C1
+SO3:
+	CMP  BL, 90
+	JBE OP4
+	ADD EDI, 1H
+	JMP OP4C1
+
+OP4:
+	XOR AX, AX
+	XOR BX, BX
+	XOR CX, CX
+	XOR DX, DX
+	MOV AL, [EDI]
+	MOV numOP2, AL
+	SUB numOP2, 65
+	MOV BL, [ESI]
+	SUB BL, 65
+	MOV numOP22, BL
+	MOV posicion, 0h
+	CMP BL, numOP2
+	JE OP4C2
+	CMP BL, numOP2
+	JA OP4C3
+	CMP BL, numOP2
+	JB OP4C4
+OP4C2: ;IGUALES
+	MOV AL, numOP2
+	MOV posicion, AL
+	JMP OP4C5
+OP4C3: ;MAYOR
+	MOV AL, numOP22
+	SUB AL, numOP2
+	MOV posicion, AL
+	JMP OP4C5
+OP4C4: ;Menor
+	MOV AL, 26
+	SUB AL, numOP2
+	ADD AL, numOP22
+	MOV posicion, AL
+OP4C5:
+
+
+	Busca_posicion posicion
+	XOR EAX, EAX
+	LEA ESI, cripto
+	MOV AL, S
+	ADD ESI, EAX
+
+	ADD EDI, 1
+	ADD ESI, 1
+	INC S
+
+	MOV AL, [ESI]
+	CMP AL, 0
+	JNE OP4C1
+SALEOP4:
+JMP EXIT_Main
 Encrypt_1 PROC Near
     XOR AX, AX
 	XOR BX, BX 
